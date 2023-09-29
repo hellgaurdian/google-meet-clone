b5ff654d3d774c6584418675a1d109dac00814e9
VishwaVijay <vishwavijay.bisht@techchefz.com>
2023-09-29 18:08:36 +0530

    added login

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+require("./Modals/UserModal");
+
+mongoose
+  .connect(
+    "mongodb+srv://vishwavijay:6cOOjfp3JvEji2J6@cluster0.p3vfbqx.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   // Serve your ad here
   res.send("Hello, world!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(6969, () => {
+  console.log("started");
 });
+
+// 6cOOjfp3JvEji2J6
+// vishwavijay
