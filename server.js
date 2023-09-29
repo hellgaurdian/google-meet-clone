127c5d19cea5321e124e588558ca519c639d167d
VishwaVijay <vishwavijay.bisht@techchefz.com>
2023-09-29 15:46:43 +0530

    vercel test

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 const express = require("express");
 
 const app = express();
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-  });
-}
+
 app.get("/", (req, res) => {
   // Serve your ad here
   res.send("Hello, world!");
